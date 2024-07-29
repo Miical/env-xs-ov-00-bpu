16441214238d82820cde43ac9aaa7286c5bdefba
Miical <879754743@qq.com>
2024-07-29 19:48:06 +0800

    Add some case

--- tests/ras-mlvp/tests/test_ras.py
+++ tests/ras-mlvp/tests/test_ras.py
@@ -39,12 +39,28 @@ async def test_ras_spec_push_and_pop_with_overflow(ras_env: RASEnv):
     for _ in range(2):
         await ras_env.s2_s3_same(gen.random_ret())
 
+async def test_ras_spec_push_and_pop_with_overflow(ras_env: RASEnv):
+    """
+    测试 RAS Spec 栈在压入相同地址栈满的情况下是否可以正常出栈
+    """
+
+    await ras_env.reset()
+
+    gen = FullPredGenerator()
+    for _ in range(SPEC_MAX_SIZE):
+        await ras_env.s2_s3_same(gen.random_call(specific_addr=0x12345))
+
+    for _ in range(SPEC_MAX_SIZE):
+        await ras_env.s2_s3_same(gen.random_ret())
+
 
 async def test_ras_test_push_same_addr_with_stack_full(ras_env: RASEnv):
     """
-    测试 RAS Counter 基础功能是否正确
+    测试 RAS Spec 栈 Counter 在出入栈时是否正确
 
-    Spec栈 Counter 有问题但未修复， RM 实现为不带 Counter 的版本
+    Spec栈 Counter 有问题但未修复, RM 实现为不带 Counter 的版本, 因此出现栈溢出
+
+    BUG 栈溢出时, POP 出的地址有异常
     """
 
     await ras_env.reset()
@@ -52,7 +68,23 @@ async def test_ras_test_push_same_addr_with_stack_full(ras_env: RASEnv):
     gen = FullPredGenerator()
     for i in range(SPEC_MAX_SIZE):
         for _ in range(MAX_COUNTER):
-            await ras_env.s2_s3_same(gen.random_call(i))
+            await ras_env.s2_s3_same(gen.random_call(specific_addr=i))
+
+    for _ in range(MAX_COUNTER):
+        await ras_env.s2_s3_same(gen.random_ret())
+
+async def test_ras_spec_pop_with_one_element(ras_env: RASEnv):
+    """
+    测试 RAS Spec 栈只有一个元素时是否可以正常出栈
+    """
+
+    await ras_env.reset()
+
+    gen = FullPredGenerator()
+    for _ in range(100):
+        await ras_env.s2_s3_same(gen.random_call())
+        await ras_env.s2_s3_same(gen.random_ret())
+
 
 
 
@@ -72,7 +104,9 @@ async def top_test(ras):
 
     # await test_ras_spec_push_and_pop(env)
     # await test_ras_spec_push_and_pop_with_overflow(env)
-    await test_ras_test_push_same_addr_with_stack_full(env)
+    # await test_ras_spec_push_and_pop_with_overflow(env)
+    # await test_ras_test_push_same_addr_with_stack_full(env)
+    # await test_ras_spec_pop_with_one_element(env)
 
 
 
